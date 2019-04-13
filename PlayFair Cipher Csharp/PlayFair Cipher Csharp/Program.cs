41b53a16d8852dbcbf36f7e32e7b8a2b43569a11
Albin <albinqerimi1999@gmail.com>
2019-04-13 11:04:51 +0200

    dekriptimi

--- PlayFair Cipher Csharp/PlayFair Cipher Csharp/Program.cs
+++ PlayFair Cipher Csharp/PlayFair Cipher Csharp/Program.cs
@@ -18,6 +18,9 @@ namespace PlayFair_Cipher_Csharp
             string key ="abcdefghiklmnopqrstuvwxyz";
             string cipherText = Playfair.Enkriptim(key, plainText);
             Console.WriteLine("Mesazhi i enkriptuar: " + cipherText);
+            plainText = Playfair.Dekriptimi(key, cipherText);
+            Console.WriteLine("Mesazhi i dekriptuar: "+plainText);
+            Console.ReadLine();
         }
     }
     public class Playfair
@@ -32,6 +35,7 @@ namespace PlayFair_Cipher_Csharp
             for (int i = 0; i < length; i++)
             {
                 char c = Text[i];
+
                 if (sb.Length % 2 == 1 && sb[sb.Length - 1] == c)
                 {
                     sb.Append('x');
@@ -115,6 +119,69 @@ namespace PlayFair_Cipher_Csharp
                     {
                         sb.Append(key[a_pozita + 5]);
                         sb.Append(key[b_pozita + 5]);
+                    }
+                }
+                else
+                {
+                    sb.Append(key[5 * a_row + b_col]);
+                    sb.Append(key[5 * b_row + a_col]);
+                }
+            }
+            return sb.ToString();
+        }
+        public static string Dekriptimi(string key, string cipherText)
+        {
+            int length = cipherText.Length;
+            char a, b;
+            int a_pozita, b_pozita, a_row, b_row, a_col, b_col;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < length; i += 2)
+            {
+                a = cipherText[i];
+                b = cipherText[i + 1];
+
+                a_pozita = key.IndexOf(a);
+                b_pozita = key.IndexOf(b);
+                a_row = a_pozita / 5;
+                b_row = b_pozita / 5;
+                a_col = a_pozita % 5;
+                b_col = b_pozita % 5;
+
+                if (a_row == b_row)
+                {
+                    if (a_col == 0)
+                    {
+                        sb.Append(key[a_pozita + 4]);
+                        sb.Append(key[b_pozita - 1]);
+                    }
+                    else if (b_col == 0)
+                    {
+                        sb.Append(key[a_pozita - 1]);
+                        sb.Append(key[b_pozita + 4]);
+                    }
+                    else
+                    {
+                        sb.Append(key[a_pozita - 1]);
+                        sb.Append(key[b_pozita - 1]);
+                    }
+                }
+                else if (a_col == b_col)
+                {
+                    if (a_row == 0)
+                    {
+                        sb.Append(key[a_pozita + 20]);
+                        sb.Append(key[b_pozita - 5]);
+                    }
+                    else if (b_row == 0)
+                    {
+                        sb.Append(key[a_pozita - 5]);
+                        sb.Append(key[b_pozita + 20]);
+                    }
+                    else
+                    {
+                        sb.Append(key[a_pozita - 5]);
+                        sb.Append(key[b_pozita - 5]);
                     }
                 }
                 else
